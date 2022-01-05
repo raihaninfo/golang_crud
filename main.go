2e21d4c3aef387edfebd039aa28e802fd4c4a03d
raihaninfo <raihanmahmudi35@gmail.com>
2022-01-05 22:43:59 +0600

    create home route

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 
 	"github.com/gorilla/mux"
 )
@@ -10,5 +11,13 @@ import (
 func main() {
 	fmt.Println("Crud app")
 	r := mux.NewRouter()
+	r.PathPrefix("/asset/").Handler(http.StripPrefix("/asset/", http.FileServer(http.Dir("view/fron-end/asset"))))
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		temp, err := template.ParseFiles("view/fron-end/index.gohtml", "view/fron-end/header.gohtml")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		temp.Execute(w, nil)
+	})
 	http.ListenAndServe(":8080", r)
 }
