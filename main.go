76dff69e8249b379aba9b4c94450a620b66a26b4
raihaninfo <raihanmahmudi35@gmail.com>
2022-01-06 16:00:54 +0600

    create home view

--- main.go
+++ main.go
@@ -3,30 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 
 	"github.com/gorilla/mux"
+	"github.com/raihaninfo/golang_crud/views"
 )
 
 var (
-	homeTemplate *template.Template
+	homeView *views.View
 )
 
 func main() {
-	var err error
-	homeTemplate, err = template.ParseFiles("view/view.gohtml")
-	if err != nil {
-		panic(err)
-	}
+	homeView = views.NewView("views/fron-end/index.gohtml")
+
 	fmt.Println("Listening port :8080")
 	r := mux.NewRouter()
-	r.PathPrefix("/asset/").Handler(http.StripPrefix("/asset/", http.FileServer(http.Dir("view/fron-end/asset"))))
+	r.PathPrefix("/asset/").Handler(http.StripPrefix("/asset/", http.FileServer(http.Dir("views/fron-end/asset"))))
 	r.HandleFunc("/", home)
 	http.ListenAndServe(":8080", r)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	err := homeTemplate.Execute(w, nil)
+	err := homeView.Template.Execute(w, nil)
 	if err != nil {
 		panic(err)
 	}
