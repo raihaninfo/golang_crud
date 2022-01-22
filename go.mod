28d96d566460c609e9acfc8dd96e4e4c729252e7
raihaninfo <raihanmahmudi35@gmail.com>
2022-01-22 22:35:41 +0600

    update fronend

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require github.com/gorilla/mux v1.8.0
 
 require (
 	github.com/gorilla/securecookie v1.1.1 // indirect
-	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // 
 )
--- views/view.go
+++ views/view.go
@@ -7,7 +7,7 @@ type View struct {
 }
 
 func NewView(files ...string) *View {
-	files = append(files, "views/fron-end/header.gohtml")
+	files = append(files, "views/fron-end/header.gohtml", "views/fron-end/menu.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
