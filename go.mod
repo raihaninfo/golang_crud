e446170acd0db39e465e0275ccac8b959fb444f0
raihaninfo <raihanmahmudi35@gmail.com>
2022-01-22 19:13:40 +0600

    modified html file

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/raihaninfo/golang_crud
 go 1.17
 
 require github.com/gorilla/mux v1.8.0
+
+require (
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"github.com/raihaninfo/golang_crud/views"
 )
 
@@ -14,7 +15,10 @@ var (
 	ubdateView   *views.View
 	deleteView   *views.View
 	notFountView *views.View
+	loginView    *views.View
 )
+
+var store = sessions.NewCookieStore([]byte("secret-password"))
 
 func main() {
 	homeView = views.NewView("views/fron-end/index.gohtml")
@@ -22,9 +26,11 @@ func main() {
 	ubdateView = views.NewView("views/fron-end/update.gohtml")
 	deleteView = views.NewView("views/fron-end/delete.gohtml")
 	notFountView = views.NewView("views/fron-end/notfount.gohtml")
+	loginView = views.NewView("views/fron-end/login.gohtml")
 	r := mux.NewRouter()
 	r.PathPrefix("/asset/").Handler(http.StripPrefix("/asset/", http.FileServer(http.Dir("views/fron-end/asset"))))
 	r.NotFoundHandler = http.HandlerFunc(notFount)
+	r.HandleFunc("/login", login)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/add", add)
 	r.HandleFunc("/update", update)
@@ -32,6 +38,17 @@ func main() {
 
 	fmt.Println("Listening port :8080")
 	http.ListenAndServe(":8080", r)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "login-session")
+	FeatchError(err)
+	_, ok := session.Values["session"]
+	if ok {
+		http.Redirect(w, r, "/", http.StatusNotFound)
+	}
+	er := loginView.Template.Execute(w, nil)
+	FeatchError(er)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
